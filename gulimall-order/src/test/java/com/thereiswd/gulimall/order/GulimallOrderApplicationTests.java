2597e4e787c1f3dd7b3cf69bbf443f763c73962d
ThereIsAW <2647832021@qq.com>
2024-05-19 13:42:54 +0800

    gulimall 0.0.0 初始版本

---
+++ gulimall-order/src/test/java/com/thereiswd/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thereiswd.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
